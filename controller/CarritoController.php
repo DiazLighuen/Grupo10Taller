eb724ff3344e124541fee628db70cf2314143dd5
Natalia Galiano <na.galiano@gmail.com>
2017-12-04 20:16:21 -0300

    agrego merge de metodos pisados

--- controller/CarritoController.php
+++ controller/CarritoController.php
@@ -1,9 +1,5 @@
 <?php
 
-/**
- * Description of AutomovilController
- *
- */
 class CarritoController extends BaseController{
 
     private static $instance;
@@ -22,7 +18,25 @@ class CarritoController extends BaseController{
 
     }
 
-    public function listar_carrito(){
+    public function agregar_a_carrito($data){
+			
+		// creo el carrito a mano ahora para probar, ya que no tengo en la sesion el id del usuario logueado
+		$carrito_id = CarritoRepository::getInstance()->crear_carrito();
+		
+		echo var_dump($carrito_id);
+		
+        // agregamos al carrito el servicio
+        if ($this->is_method_post()){
+            $service_id = $data['id'];
+            $type = $data['type'];
+            $service_id_nuevo = CarritoRepository::getInstance()->agregar_a_carrito($service_id,$type,$carrito_id);
+			echo var_dump($service_id_nuevo);
+        }
+		
+		die;
+    }
+	
+	public function listar_carrito(){
       if ($this->is_method_post()){
         //pagar_carrito()
       }
--- model/CarritoRepository.php
+++ model/CarritoRepository.php
@@ -1,8 +1,5 @@
 <?php
 
-/**
- * Class LoginRepository
- */
 class CarritoRepository extends PDORepository{
 
     private static $instance;
@@ -20,6 +17,31 @@ class CarritoRepository extends PDORepository{
 
     }
 
+    public function crear_carrito(){
+      $con = $this->getConnection ();
+      $sql = 'INSERT INTO cart (date, price) VALUES (NOW(), 0)';
+      $stmt = $con->prepare ( $sql );
+      $stmt->execute ();
+      return $con->lastInsertId();
+    }
+	
+	public function agregar_a_carrito($service_id, $type, $cart_id){
+	  $con = $this->getConnection ();
+      $sql = 'INSERT INTO services (cart_id, service_id, type) VALUES (:cart_id, :service_id, :type)';
+      $stmt = $con->prepare ( $sql );
+	  $stmt->bindParam (':cart_id', $cart_id, PDO::PARAM_INT );
+	  $stmt->bindParam (':service_id', $service_id, PDO::PARAM_INT );
+	  $stmt->bindParam (':type', $type, PDO::PARAM_STR );
+	  $service_id = (int) $service_id;
+	  $cart_id = (int) $cart_id;
+      $stmt->execute ();
+	  echo var_dump($service_id);
+	  echo var_dump($type);
+	  echo var_dump($cart_id);
+	  
+      return $con->lastInsertId();
+	}
+	
     public function obtener_servicios_carrito(){
       // para probar, tengo que hacer la consulta adecuada
       $con = $this->getConnection ();
@@ -121,6 +143,6 @@ class CarritoRepository extends PDORepository{
       $stmt->execute ();
       $servicio_detalle = $stmt->fetchAll ();
       return $servicio_detalle;
-    }
+    }	
 
 }
