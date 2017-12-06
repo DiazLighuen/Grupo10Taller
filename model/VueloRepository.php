019192b9f7e678b5efcf7e528fec2b09ba5b54f6
Diaz Lighuen <lighuen.diaz@gmail.com>
2017-12-06 12:39:05 -0300

    fix listar busqueda de vuelos name

--- model/VueloRepository.php
+++ model/VueloRepository.php
@@ -22,7 +22,7 @@ class VueloRepository extends PDORepository{
 
     public function buscar_vuelo($fecha_desde,$fecha_hasta,$ciudad_origen_id,$ciudad_destino_id){
       $con = $this->getConnection ();
-      $sql = 'SELECT s.id as id_seat, s.number, s.class, s.price, u.username as nombre_aerolinea, f.origin_date, f.destiny_date FROM seat s, flight f, user u where s.flight_id = f.id and u.id = f.airline_id and s.sell = 0 and f.origin_id = :ciudad_origen_id and f.destiny_id = :ciudad_destino_id and (:fecha_desde BETWEEN origin_date and destiny_date or :fecha_hasta BETWEEN origin_date and destiny_date)';
+      $sql = 'SELECT s.id as id_seat, s.number, s.class, s.price, u.name as nombre_aerolinea, f.origin_date, f.destiny_date FROM seat s, flight f, user u where s.flight_id = f.id and u.id = f.airline_id and s.sell = 0 and f.origin_id = :ciudad_origen_id and f.destiny_id = :ciudad_destino_id and (:fecha_desde BETWEEN origin_date and destiny_date or :fecha_hasta BETWEEN origin_date and destiny_date)';
       $stmt = $con->prepare ( $sql );
       $stmt->bindParam (':ciudad_origen_id', $ciudad_origen_id, PDO::PARAM_INT);
 	  $stmt->bindParam (':ciudad_destino_id', $ciudad_destino_id, PDO::PARAM_INT);
